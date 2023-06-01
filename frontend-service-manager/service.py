d6c3f91f547ad33a725a1d4f155a8ab5ff4a9fa7
Janne Kerola <janne.kerola@oulu.fi>
2023-06-01 13:31:29 +0300

    remove dockerfile, add argparser to worker

--- frontend-service-manager/service.py
+++ frontend-service-manager/service.py
@@ -4,16 +4,17 @@ import psutil
 import logging
 import subprocess
 import os
-
+from argparse import ArgumentParser
 from client import Connection
 
 
-FORMAT = '%(asctime)s %(levelname)s %(message)s'
-logging.basicConfig(#filename="backend-service.log",
-                    format=FORMAT,
-                    level=logging.DEBUG)
+FORMAT = "%(asctime)s %(levelname)s %(message)s"
+logging.basicConfig(  # filename="backend-service.log",
+    format=FORMAT, level=logging.DEBUG
+)
 
 PROCESS = None
+
 
 def download_task(task_name):
     """
@@ -21,21 +22,14 @@ def download_task(task_name):
     """
     base_address = "http://localhost:5001/api/v1"
     url = base_address + "/tasks/" + task_name + "/download"
-    
+
     # create a directory to store the task tag it with the timestamp
     task_dir = os.curdir + "/.tasks/" + task_name + "-" + str(int(time.time()))
     os.makedirs(task_dir, exist_ok=True)
 
     process = subprocess.run(
-            [    
-                "curl",
-                url, 
-                "-JO", 
-                "-w \"%{http_code}\""
-            ],
-            capture_output=True,
-            cwd=task_dir
-        )
+        ["curl", url, "-JO", '-w "%{http_code}"'], capture_output=True, cwd=task_dir
+    )
     if process.returncode != 0:
         logging.error(f"error downloading task: {task_name}", process.stderr)
         return 0
@@ -57,34 +51,49 @@ async def run_task(socket: Connection, task_name, task_args, return_type) -> Non
 
     logging.info(f"running task: {task_name}")
     await socket.send("task-running", {"task_name": task_name})
-    task_dir = [dir
-        for dir in os.listdir(os.getcwd() + "/.tasks/") if (task_name in dir and "venv" != dir)
-        ]
+    task_dir = [
+        dir
+        for dir in os.listdir(os.getcwd() + "/.tasks/")
+        if (task_name in dir and "venv" != dir)
+    ]
     task_dir.sort()
     task_dir = os.getcwd() + "/.tasks/" + task_dir[-1]
     python_dir = os.getcwd() + "/.tasks/" + "venv/bin"
     task_args = task_args.split(" ")
     try:
         PROCESS = await asyncio.subprocess.create_subprocess_exec(
-            
-            python_dir + "/python", 
+            python_dir + "/python",
             *task_args,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
-            cwd = task_dir
+            cwd=task_dir,
         )
-        
+
         stdout, stderr = await PROCESS.communicate()
 
         if PROCESS.returncode == 0:
-            logging.info(f"task: {task_name} completed with return code: {PROCESS.returncode}")
-            await socket.send("task-finished", {"task_name": task_name, "return_value": stdout.decode()})
+            logging.info(
+                f"task: {task_name} completed with return code: {PROCESS.returncode}"
+            )
+            await socket.send(
+                "task-finished",
+                {"task_name": task_name, "return_value": stdout.decode()},
+            )
         elif PROCESS.returncode == -15:
-            logging.info(f"task: {task_name} terminated with return code: {PROCESS.returncode}")
-            await socket.send("task-stopped", {"task_name": task_name, "return_value": stdout.decode()})
+            logging.info(
+                f"task: {task_name} terminated with return code: {PROCESS.returncode}"
+            )
+            await socket.send(
+                "task-stopped",
+                {"task_name": task_name, "return_value": stdout.decode()},
+            )
         else:
-            logging.error(f"task: {task_name} failed with return code: {PROCESS.returncode}")
-            await socket.send("task-failed", {"task_name": task_name, "return_value": stderr.decode()})
+            logging.error(
+                f"task: {task_name} failed with return code: {PROCESS.returncode}"
+            )
+            await socket.send(
+                "task-failed", {"task_name": task_name, "return_value": stderr.decode()}
+            )
 
         PROCESS = None
 
@@ -104,17 +113,40 @@ def stop_task():
         logging.warning("no task running")
 
 
+def create_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-a",
+        "--address",
+        default="localhost",
+        required=False,
+        action="store",
+        dest="host",
+        help="host ip address",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        default="5556",
+        required=False,
+        action="store",
+        dest="port",
+        help="port number",
+    )
+    return parser
+
+
 async def send_info(socket: Connection) -> None:
     # gets some information about the system
     # such as cpu usage, memory usage, etc.
     # sends info to server
     await asyncio.sleep(2)
     while True:
-        cpu_usage =  psutil.cpu_percent()
+        cpu_usage = psutil.cpu_percent()
         memory_usage = psutil.virtual_memory().percent
-        await socket.send("info", {"cpu": cpu_usage, "memory": memory_usage}) 
+        await socket.send("info", {"cpu": cpu_usage, "memory": memory_usage})
         await asyncio.sleep(60)
-        
+
 
 async def heartbeat(socket: Connection) -> None:
     await asyncio.sleep(1.6)
@@ -142,8 +174,13 @@ async def handler(socket: Connection) -> None:
                 print(data)
                 # create a task
                 asyncio.create_task(
-                    run_task(socket, data[3]["task_name"], data[3]["args_to_run"], data[3]["return_type"])
+                    run_task(
+                        socket,
+                        data[3]["task_name"],
+                        data[3]["args_to_run"],
+                        data[3]["return_type"],
                     )
+                )
             elif data[0] == "stop-task":
                 stop_task()
             else:
@@ -171,18 +208,20 @@ def create_virtual_environment() -> None:
     logging.info("creating virtual environment")
     subprocess.run(["bash", "init.sh"])
     logging.info("virtual environment created!")
-    
+
 
 if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
     create_virtual_environment()
+
     socket = Connection()
+    if args.host:
+        socket._address = args.host
+    if args.port and int(args.port):
+        socket._port = int(args.port)
     asyncio.run(
         asyncio.wait(
-            [
-                socket.connect(),
-                heartbeat(socket),
-                handler(socket),
-                send_info(socket)
-            ]
+            [socket.connect(), heartbeat(socket), handler(socket), send_info(socket)]
         )
     )
